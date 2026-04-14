6eb4a9044618155fc6c739bec26563f8b315a865
txchacal-lang <txchacal@gmail.com>
2026-04-14 14:16:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 
